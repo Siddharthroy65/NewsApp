b16ca3690b1b58af69595f3d93819163e30f0c9c
Siddharth Roy <siddharth@prosoltechline.com>
2022-08-02 22:09:55 +0530

    Dark/ light mode change

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'components/customListTile.dart';
 import 'model/article_model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:NewsApp/prefs/style.dart' as Style;
+import 'package:NewsApp/prefs/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 var dbDeleted = 0;
 
@@ -16,28 +19,36 @@ void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class _MyAppState extends State<MyApp> {
+  bool switchValue = false;
+  ThemeProvider themeProvider = ThemeProvider();
   ApiService client = ApiService();
 
+  void getCurrentTheme() async {
+    themeProvider.darkTheme = await themeProvider.preference.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentTheme();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return ChangeNotifierProvider(
+      create: (_) => themeProvider,
+      child: Consumer<ThemeProvider>(
+        builder: (context, value, child){
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: Style.Style.themeData(themeProvider.darkTheme),
+            home: Scaffold(
     appBar: AppBar(leading: IconButton(onPressed: () {
      
    }, icon: Icon(Icons.menu,color: Colors.black,)),
@@ -46,57 +57,54 @@ class _HomePageState extends State<HomePage> {
    title: Text("News Headline",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20),),
     actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.notifications,color: Colors.red,),
-            
-          ), //IconButton
+            icon: const Icon(Icons.notifications,color: Colors.red,), onPressed: () {  },
+            ),
+          Switch(
+            value: switchValue, 
+            onChanged: (val){
+              themeProvider.darkTheme = !themeProvider.darkTheme;
+              setState(() {
+                switchValue = val;
+              });
+            })
         
         ],
    ),
-    
-      body:Column(
+    body: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            child: SingleChildScrollView(
-              physics: AlwaysScrollableScrollPhysics(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                 Padding(
-                   padding: const EdgeInsets.all(5.0),
-                   child: Text('Trending Topics',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 20,),),
-                 ),
-                  FutureBuilder(
-                    
-                    future: client.getArticle(),
-                    builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
-                      if (dbDeleted == 0){
-                        deleteDBOnStart().then((value) => value);
-                        dbDeleted=1;
-                      }
-                      if (snapshot.hasData) {
-                      
-                        List<Article> articles = snapshot.data;
-                        return ListView.builder(
-                    shrinkWrap: true,
-                          itemCount: articles.length,
-                          itemBuilder: (context, index) =>
-                              customListTile(articles[index], context),
-                        );
-                      }
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    },
-                    
-                  ),
-                ],
-              ),
-            ),
+      
+          FutureBuilder(
+            
+            future: client.getArticle(),
+            builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+              if (dbDeleted == 0){
+                deleteDBOnStart().then((value) => value);
+                dbDeleted=1;
+              }
+              if (snapshot.hasData) {
+              
+                List<Article> articles = snapshot.data;
+                return ListView.builder(
+            shrinkWrap: true,
+                  itemCount: articles.length,
+                  itemBuilder: (context, index) =>
+                      customListTile(articles[index], context),
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+            
           ),
         ],
       ),
-   
-      
+    )));
+        },
+      ),
     );
   }
 }
+
