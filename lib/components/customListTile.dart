6f88900d0cfa8e140c2cb617144e931e09a6b411
Siddharth Roy <siddharth@prosoltechline.com>
2022-08-04 16:18:59 +0530

    search box or tittle

--- lib/components/customListTile.dart
+++ lib/components/customListTile.dart
@@ -4,68 +4,45 @@ import 'package:flutter/material.dart';
 
 Widget customListTile(Article article, BuildContext context) {
   return InkWell(
-    onTap: () {
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => ArticlePage(
-                    article: article,
-                  )));
-    },
-    child: Container(
-      margin: EdgeInsets.all(12.0),
-      padding: EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 3.0,
-            ),
-          ]),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 150.0,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              
-              image: DecorationImage(
-                  image: NetworkImage(article.urlToImage), fit: BoxFit.cover),
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          Container(
-            padding: EdgeInsets.all(6.0),
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(30.0),
-            ),
-            child: Text(
-              article.source.name,
-              style: TextStyle(
-                color: Colors.white,
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ArticlePage(
+                      article: article,
+                    )));
+      },
+      child: Container(
+        margin: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 1.0,
+              ),
+            ]),
+        child: ListTile(
+          leading: SizedBox(
+            child: Container(
+              height: 300.0,
+              child: Image.network(
+                article.urlToImage,
+                fit: BoxFit.cover,
               ),
             ),
           ),
-          SizedBox(
-            height: 8.0,
-          ),
-          Text(
+          title: Text(
             article.title,
             style: TextStyle(
               fontWeight: FontWeight.bold,
-              fontSize: 16.0,
+              fontSize: 15.0,
             ),
-          )
-        ],
-      ),
-    ),
-  );
+          ),
+          subtitle: Text(
+            article.source.name,
+          ),
+        ),
+      ));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ Future<void> deleteDBOnStart() async {
   deleteDatabase(path);
 }
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -44,67 +44,109 @@ class _MyAppState extends State<MyApp> {
     return ChangeNotifierProvider(
       create: (_) => themeProvider,
       child: Consumer<ThemeProvider>(
-        builder: (context, value, child){
+        builder: (context, value, child) {
           return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: Style.Style.themeData(themeProvider.darkTheme),
-            home: Scaffold(
-    appBar: AppBar(leading: IconButton(onPressed: () {
-     
-   }, icon: Icon(Icons.menu,color: Colors.black,)),
-   backgroundColor: Color(0xFFFAFAFA),
-   centerTitle: true,
-   title: Text("News Headline",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20),),
-    actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.notifications,color: Colors.red,), onPressed: () {  },
-            ),
-          Switch(
-            value: switchValue, 
-            onChanged: (val){
-              themeProvider.darkTheme = !themeProvider.darkTheme;
-              setState(() {
-                switchValue = val;
-              });
-            })
-        
-        ],
-   ),
-    body: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-      
-          FutureBuilder(
-            
-            future: client.getArticle(),
-            builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
-              if (dbDeleted == 0){
-                deleteDBOnStart().then((value) => value);
-                dbDeleted=1;
-              }
-              if (snapshot.hasData) {
-              
-                List<Article> articles = snapshot.data;
-                return ListView.builder(
-            shrinkWrap: true,
-                  itemCount: articles.length,
-                  itemBuilder: (context, index) =>
-                      customListTile(articles[index], context),
-                );
-              }
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            },
-            
-          ),
-        ],
-      ),
-    )));
+              debugShowCheckedModeBanner: false,
+              theme: Style.Style.themeData(themeProvider.darkTheme),
+              home: Scaffold(
+                  appBar: AppBar(
+                    leading: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.menu,
+                          color: Colors.black,
+                        )),
+                    backgroundColor: Color(0xFFFAFAFA),
+                    centerTitle: true,
+                    title: Text(
+                      "News Headline",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                    actions: <Widget>[
+                      IconButton(
+                        icon: const Icon(
+                          Icons.notifications,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {},
+                      ),
+                      Switch(
+                          value: switchValue,
+                          onChanged: (val) {
+                            themeProvider.darkTheme = !themeProvider.darkTheme;
+                            setState(() {
+                              switchValue = val;
+                            });
+                          })
+                    ],
+                  ),
+                  body: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text("CNBC",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text("HEY, Jon",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: Colors.grey),),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text("Discover Latest News",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 45),),
+                          ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                hintText: 'Search for news',
+                                hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                                suffixIcon: Container(
+                                  color: Colors.red,
+                                  child:
+                                      Icon(Icons.search, color: Colors.white),
+                                )),
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+             Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Text('Breaking News',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+             ),
+                        FutureBuilder(
+                          future: client.getArticle(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<List<Article>> snapshot) {
+                            if (dbDeleted == 0) {
+                              deleteDBOnStart().then((value) => value);
+                              dbDeleted = 1;
+                            }
+                            if (snapshot.hasData) {
+                              List<Article> articles = snapshot.data;
+                              return ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: articles.length,
+                                itemBuilder: (context, index) =>
+                                    customListTile(articles[index], context),
+                              );
+                            }
+                            return Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  )));
         },
       ),
     );
   }
 }
-
